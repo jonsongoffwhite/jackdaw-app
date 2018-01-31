13e021afd37e42bc4646ad543b30d9fd9933965b
jhg14 <jonnygoffwhite@gmail.com>
2018-01-31 23:39:58 +0000

    Initial Commit

---
+++ MenuBarApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenuBarApp
+//
+//  Created by Jonson Goff-White on 31/01/2018.
+//  Copyright © 2018 Jonson Goff-White. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MenuBarApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuBarApp
+//
+//  Created by Jonson Goff-White on 31/01/2018.
+//  Copyright © 2018 Jonson Goff-White. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
