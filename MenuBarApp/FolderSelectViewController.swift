27cd3531abbef18b0a3d7fc3571b55e2dce15594
jhg14 <jonnygoffwhite@gmail.com>
2018-04-16 13:46:49 +0100

    Update .gitignore

---
+++ MenuBarApp/FolderSelectViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FolderSelectViewController.swift
+//  MenuBarApp
+//
+//  Created by Jonson Goff-White on 16/04/2018.
+//  Copyright © 2018 Jonson Goff-White. All rights reserved.
+//
+
+import Foundation
---
+++ MenuBarApp/GitCommandsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GitCommandsViewController.swift
+//  MenuBarApp
+//
+//  Created by Jonson Goff-White on 14/04/2018.
+//  Copyright © 2018 Jonson Goff-White. All rights reserved.
+//
+
+import Cocoa
+
+class GitCommandsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
