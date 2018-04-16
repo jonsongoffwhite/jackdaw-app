735c20bf1cbbb385dc90fa55a8785f5a44a7350a
jhg14 <jonnygoffwhite@gmail.com>
2018-04-16 13:47:21 +0100

    Implement file selection

--- MenuBarApp/FolderSelectViewController.swift
+++ MenuBarApp/FolderSelectViewController.swift
@@ -6,4 +6,62 @@
 //  Copyright © 2018 Jonson Goff-White. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+class FolderSelectViewController: NSViewController {
+    
+    @IBOutlet weak var selectedDir: NSTextField?
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    @IBAction func browseFile(sender: AnyObject) {
+        
+        let dialog = NSOpenPanel()
+        
+        dialog.title = "Select project folder"
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseDirectories    = true;
+        dialog.canCreateDirectories    = false;
+        dialog.allowsMultipleSelection = false;
+        // dialog.allowedFileTypes        = [];
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            guard let directory = dialog.url else {
+                return
+            }
+            selectedDir?.stringValue = directory.path
+        } else {
+            return
+        }
+    }
+    
+    @IBAction func confirm(sender: AnyObject) {
+        performSegue(withIdentifier: NSStoryboardSegue.Identifier(rawValue: "showGitCommands"), sender: sender)
+    }
+    
+    override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
+        if segue.destinationController is GitCommandsViewController {
+            let vc = segue.destinationController as! GitCommandsViewController
+            vc.directory = selectedDir?.stringValue
+        }
+    }
+    
+}
+
+extension FolderSelectViewController {
+    // MARK: Storyboard instantiation
+    static func freshController() -> FolderSelectViewController {
+        // Get Main story board
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        
+        // Get ViewController with identifier GitCommandsViewController
+        let identifier = NSStoryboard.SceneIdentifier(rawValue: "FolderSelectViewController")
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? FolderSelectViewController else {
+            fatalError("Why cant i find FolderSelectViewController? - Check Main.storyboard")
+        }
+        return viewcontroller
+    }
+}
--- MenuBarApp/GitCommandsViewController.swift
+++ MenuBarApp/GitCommandsViewController.swift
@@ -9,10 +9,53 @@
 import Cocoa
 
 class GitCommandsViewController: NSViewController {
+    
+    @IBOutlet weak var projectName: NSTextField!
+    @IBOutlet weak var abletonStatus: NSTextField?
+    @IBOutlet weak var refreshStatusButton: NSButton?
+    
+    //TODO: Check it has .git subdir
+    var directory: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        refreshStatusButton?.action = #selector(refreshStatus(_:))
+        
+        if let dir = directory {
+            projectName.stringValue = String(dir.split(separator: "/").last!)
+            projectName.stringValue += ", is Git: \(isGitDirectory())"
+        }
+        
+    }
+    
+    /*
+    ** Determines whether the provided directory is yet a git directory
+    ** by checking for the existence of the .git hidden directory
+    ** present in all git repositories
+    */
+    func isGitDirectory() -> Bool {
+        guard let dir = directory else {
+            return false
+        }
+        let manager = FileManager()
+        return manager.fileExists(atPath: dir + "/.git")
+    }
+    
+    @objc func refreshStatus(_ sender: Any?) {
+        let task = Process()
+        task.launchPath = "/usr/bin/top"
+        task.arguments = []
+        let pipe = Pipe()
+        task.standardError = pipe
+        task.standardOutput = pipe
+        task.launch()
+        task.waitUntilExit()
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as! String
+        
+        print(output)
     }
     
 }
