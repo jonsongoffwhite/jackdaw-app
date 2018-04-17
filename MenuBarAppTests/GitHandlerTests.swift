d52ca3340285c0c67cd51b55cb834604e3196698
jhg14 <jonnygoffwhite@gmail.com>
2018-04-17 20:16:16 +0100

    Add basic test

--- MenuBarAppTests/GitHandlerTests.swift
+++ MenuBarAppTests/GitHandlerTests.swift
@@ -16,12 +16,18 @@ class GitHandlerTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // Setup test repo
+        Repository.create(at: URL(fileURLWithPath: "test-repo"))
         
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func testRepoPresent() {
+        let repo = Repository.at(URL(fileURLWithPath: "test-repo"))
+        XCTAssertNotNil(repo.value)
     }
 
     func testExample() {
