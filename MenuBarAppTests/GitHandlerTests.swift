221f74045847f9aa0d14d83d6a39c0827ff469e1
jhg14 <jonnygoffwhite@gmail.com>
2018-04-17 20:09:49 +0100

    Move frameworks for use in tests

---
+++ MenuBarAppTests/GitHandlerTests.swift
@@ -0,0 +1,39 @@
+//
+//  GitHandlerTests.swift
+//  MenuBarAppTests
+//
+//  Created by Jonson Goff-White on 17/04/2018.
+//  Copyright © 2018 Jonson Goff-White. All rights reserved.
+//
+
+import XCTest
+import SwiftGit2
+
+class GitHandlerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // Setup test repo
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
