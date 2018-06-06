02bce71fdd31e0999a50e39f54b9b89e0035d406
jhg14 <jonnygoffwhite@gmail.com>
2018-06-06 20:22:13 +0100

    Add skeleton version select view controller

---
+++ Jackdaw/VersionSelectViewController.swift
@@ -0,0 +1,28 @@
+//
+//  VersionSelectionViewController.swift
+//  Jackdaw
+//
+//  Created by Jonson Goff-White on 06/06/2018.
+//  Copyright © 2018 Jonson Goff-White. All rights reserved.
+//
+
+import Cocoa
+
+class VersionSelectViewController: NSViewController {
+    
+    @IBOutlet var table: NSTableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}
+
+extension VersionSelectViewController: NSTableViewDataSource, NSTableViewDelegate {
+    
+    
+    
+}
